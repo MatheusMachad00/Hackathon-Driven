75c6e2078767b84fb5453bd59a42e922a24254c7
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-03-04 20:18:16 -0300

    https bug fixed

--- Scripts/script.js
+++ Scripts/script.js
@@ -14,14 +14,14 @@ function generateHTML (answer){
     console.log(answer);
     let temp = (answer.data.main.temp - 273.15);
     const section = document.querySelector(".weather");
-    section.innerHTML += "";
+    section.innerHTML = "";
     section.innerHTML += `<p>${temp.toFixed(1)}°C, ${answer.data.weather[0].description}</p>
     <img src="http://openweathermap.org/img/wn/${answer.data.weather[0].icon}@2x.png" alt="icon">`;
 }
 
 function getLocation (){
     let userCity = document.getElementById("city").value;
-    let request = axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=1&appid=${API_KEY}`);
+    let request = axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=1&appid=${API_KEY}`);
     request.then(userLocation);
     request.catch();
 }
@@ -29,4 +29,17 @@ function getLocation (){
 function userLocation (location){
     lati = location.data[0].lat;
     long = location.data[0].lon;
+}
+
+function autoLocation(){
+    let location = confirm("Você deseja saber o clima de sua atual localização?");
+    if (location){
+        autoLocationAPI();
+    }
+}
+
+function autoLocationAPI(){
+    let request = axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${userCity}&limit=1&appid=${API_KEY}`);
+    request.then(userLocation);
+    request.catch();
 }
